c9a38a87a5bfcdd24ccc8355d2e5a2915beb8489
Josue2211 <78932497+Josue2211@users.noreply.github.com>
2021-02-21 21:27:36 -0600

    Continuacion de proyecto

---
+++ Josue02/Josue01/VISTA/FrmListaVisualDeUsuario.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Josue01.VISTA
+{
+    partial class FrmListaVisualDeUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView3.Location = new System.Drawing.Point(189, 82);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(324, 269);
+            this.dataGridView3.TabIndex = 2;
+            this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Lista de Usuarios";
+            this.Column1.Name = "Column1";
+            // 
+            // FrmListaVisualDeUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 436);
+            this.Controls.Add(this.dataGridView3);
+            this.Name = "FrmListaVisualDeUsuario";
+            this.Text = "FrmListaVisualDeUsuario";
+            this.Load += new System.EventHandler(this.FrmListaVisualDeUsuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
---
+++ Josue02/Josue01/VISTA/FrmListaVisualDeUsuario.cs
@@ -0,0 +1,38 @@
+﻿using Josue01.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Josue01.VISTA
+{
+    public partial class FrmListaVisualDeUsuario : Form
+    {
+        public FrmListaVisualDeUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmListaVisualDeUsuario_Load(object sender, EventArgs e)
+        {
+            ClsListaUsuarios cls = new ClsListaUsuarios();
+
+            foreach (var iteracion in cls.user) {
+
+                dataGridView3.Rows.Add(iteracion.ToString());
+
+            }
+
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
